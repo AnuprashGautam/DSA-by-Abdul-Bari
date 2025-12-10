c6cdf216f53b8b9aafe2a09e4d90430ad135858e
Anuprash <anuprashgautam@hotmail.com>
2025-12-10 15:32:39 +0530

    Add initial implementation of tree recursion in Test.java

---
+++ Section-5 Recursion/05 Tree Recursion/Test.java
@@ -0,0 +1,18 @@
+public class Test {
+    static void fun(int n)
+    {
+        if(n>0)
+        {
+            System.out.print(n+" ");
+
+            fun(n-1);
+            fun(n-1);
+        }
+    }
+
+    public static void main(String[] args) {
+        int x=3;
+
+        fun(x);
+    }
+}
