d6451d3325800f73aeb92e5b4f0b84b69f9a7da5
Anuprash <anuprashgautam@hotmail.com>
2026-01-13 22:46:25 +0530

    Implement binary tree creation with user input for left and right children in BinaryTreeExample.java

---
+++ Section-15 Trees/01 Creating Binary Tree/BinaryTreeExample.java
@@ -0,0 +1,60 @@
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.Scanner;
+
+class Node{
+    int val;
+    Node left;
+    Node right;
+    
+    Node(int val)
+    {
+        this.val=val;
+    }
+}
+
+public class BinaryTreeExample
+{
+    void createBinaryTree()
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("What is the root node? ");
+        int rootVal=sc.nextInt();
+
+        Node root=new Node(rootVal);
+
+        Queue<Node> q = new ArrayDeque<>();
+        q.add(root);
+
+        while(!q.isEmpty())
+        {
+            Node popedNode = q.poll();
+
+            System.out.println("What is left child for this node "+popedNode.val+" ?(-1 for none)");
+            int leftVal = sc.nextInt();
+
+            if(leftVal!=-1)
+            {
+                Node leftChild = new Node(leftVal);
+                popedNode.left=leftChild;
+                q.add(leftChild);
+            }
+
+            System.out.println("What is right child for this node "+popedNode.val+" ?(-1 for none)");
+            int rightVal = sc.nextInt();
+
+            if(rightVal!=-1)
+            {
+                Node rightChild = new Node(rightVal);
+                popedNode.right=rightChild;
+                q.add(rightChild);
+            }
+        }
+
+        System.err.println("Congratulations! Your binary tree is ready.");
+    }
+    public static void main (String[] args)
+    {
+        new BinaryTreeExample().createBinaryTree();
+    }
+}
