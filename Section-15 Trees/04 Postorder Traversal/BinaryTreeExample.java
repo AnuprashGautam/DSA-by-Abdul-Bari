fea9fa09cc6019f947766d1753e80f46fe35ee46
Anuprash <anuprashgautam@hotmail.com>
2026-01-13 23:23:40 +0530

    Implement binary tree creation and preorder traversal in BinaryTreeExample.java

---
+++ Section-15 Trees/04 Postorder Traversal/BinaryTreeExample.java
@@ -0,0 +1,77 @@
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.Scanner;
+
+class Node{
+    int val;
+    Node left;
+    Node right;
+    
+    Node(int val)
+    {
+        this.val=val;
+    }
+}
+
+public class BinaryTreeExample
+{
+    Node createBinaryTree()
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("What is the root node? ");
+        int rootVal=sc.nextInt();
+
+        Node root=new Node(rootVal);
+
+        Queue<Node> q = new ArrayDeque<>();
+        q.add(root);
+
+        while(!q.isEmpty())
+        {
+            Node popedNode = q.poll();
+
+            System.out.println("What is left child for this node "+popedNode.val+" ?(-1 for none)");
+            int leftVal = sc.nextInt();
+
+            if(leftVal!=-1)
+            {
+                Node leftChild = new Node(leftVal);
+                popedNode.left=leftChild;
+                q.add(leftChild);
+            }
+
+            System.out.println("What is right child for this node "+popedNode.val+" ?(-1 for none)");
+            int rightVal = sc.nextInt();
+
+            if(rightVal!=-1)
+            {
+                Node rightChild = new Node(rightVal);
+                popedNode.right=rightChild;
+                q.add(rightChild);
+            }
+        }
+
+        System.err.println("Congratulations! Your binary tree is ready.");
+
+        return root;
+    }
+
+    void preOrderTraversal(Node node)
+    {
+        if(node!=null)
+        {
+            System.out.print(node.val+" ");
+            preOrderTraversal(node.left);
+            preOrderTraversal(node.right);
+        }
+    }
+    public static void main (String[] args)
+    {
+        BinaryTreeExample b = new BinaryTreeExample();
+
+        Node root = b.createBinaryTree();
+
+        System.out.println("--------------------PREORDER TRAVERSAL:---------------------");
+        b.preOrderTraversal(root);
+    }
+}
